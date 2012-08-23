af2730358607724686ce56cdf7aeb427d2a12855
edooan <edward.parra@koombea.com>
2012-08-23 14:43:31 -0500

    Initial commit with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupportKoombea::Application.config.secret_token = 'd1eb528e7433f37c7738f2e8759a6a0796579f95429147aea36d7d51b25087bbdfbad526a70a467033c366376e6310380a1a1711e324de014de1b7341fe8d0df'
