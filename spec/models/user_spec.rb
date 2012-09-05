adc77158c5885789c1f4618e2b57bc95ef4a5cae
asantoya <armando.santoya@hotmail.com>
2012-09-05 14:15:05 -0500

    testing login with google apps

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe "find_for_google_apps" do
+    before do
+      @auth = {
+        provider: "google_apps",
+        uid: "http://koombea.com/openid?id=103063742222819161132",
+        info: {
+          email: "jose.gomez@koombea.com",
+          name: "Jose Gomez"
+        }.stringify_keys
+      }.stringify_keys
+    end
+
+    it "should instantiate a new user if it doesn't already exist" do
+      User.find_for_google_apps(@auth).should be_new_record
+    end
+
+    it "should assign auth's attributes to new user" do
+      @user = User.find_for_google_apps(@auth)
+      info = @auth["info"]
+      @user.email.should eql(info["email"])
+    end
+
+    it "should not create a new user if it is found on the database, by email" do
+      @user = FactoryGirl.create(:user, email: @auth["info"]["email"])
+      expect {
+        User.find_for_google_apps(@auth)
+      }.not_to change(User, :count)
+    end
+
+    it "should return the existing user if it already exists" do
+      @user = FactoryGirl.create(:user, email: @auth["info"]["email"])
+      User.find_for_google_apps(@auth).should eql(@user)
+    end
+  end
