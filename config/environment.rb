357955da73758a546c117160542780b390691e90
edooan <edward.parra@koombea.com>
2012-09-07 12:52:37 -0500

    add settings smtp

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SupportKoombea::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app7104717@heroku.com",
+  :password => "4torw8sb",
+  :domain => "heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
