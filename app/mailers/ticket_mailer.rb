1b6ade990876334d0a2324a30371117a01f95087
edooan <edward.parra@koombea.com>
2012-09-07 15:03:10 -0500

    remove ap @users

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -2,8 +2,6 @@ class TicketMailer < ActionMailer::Base
 
   @users = User.where(role: "support", receives_notifications: true).pluck(:email).join(",")
 
-  ap @users
-
   default from: "contact@koombea.com",
           to: @users
 
