332034c33963fc2b4352eef01eb952962af0f24e
edooan <edward.parra@koombea.com>
2012-09-13 10:34:09 -0500

    mail when state change

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -16,4 +16,25 @@ class TicketMailer < ActionMailer::Base
 
     mail(to: @users, subject: "New Comment")
   end
+
+  def state_change(ticket, status, user)
+    @ticket = ticket
+    @users_mail = [@ticket.assigned_to.email, @ticket.user.email].join(",") 
+    @user = user
+
+    case status
+      when "approved"
+        @status = "APPROVED"
+      when "in_process"
+        @status = "IN PROCESS"
+      when "ended"
+        @status = "ENDED"
+      when "pending"
+        @status = "PENDING"
+      when "rejected"
+          @status = "REJECTED"          
+    end
+  
+    mail(to: @users_mail, subject: "State change in a ticket")
+  end
 end
