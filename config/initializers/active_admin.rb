32d84f5ec003b3275065ac3fcaf988a19b11b114
edooan <edward.parra@koombea.com>
2012-09-13 10:08:16 -0500

    changed site title

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Support Koombea"
+  config.site_title = "KSupport"
   config.skip_before_filter :authenticate_user!
   
 
