d4913a78a182350a2f95b78d1279a2069ed2cd3f
edooan <edward.parra@koombea.com>
2012-10-03 16:22:54 -0500

    add option for to change password

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ SupportKoombea::Application.routes.draw do
 
   get "comments/destroy"
 
-  devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }, :skip => [:registrations]                                          
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
 
   authenticated :user do
     root :to => "tickets#index"
